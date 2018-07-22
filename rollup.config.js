f99fe8079b52a760e302f39472c918a325cb6cee
ksure <kyle.scherling@gmail.com>
2018-07-22 16:01:54 -0700

    Adds babel plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import resolve from 'rollup-plugin-node-resolve'
-// import commonjs from 'rollup-plugin-commonjs'
-// import babel from 'rollup-plugin-babel'
+import babel from 'rollup-plugin-babel'
+import commonjs from 'rollup-plugin-commonjs'
 
 export default {
   input: 'src/index.js',
@@ -8,18 +8,18 @@ export default {
     file: 'dist/index.js',
     format: 'cjs'
   },
-  plugins: [resolve()]
+  plugins: [
+    resolve(),
+    babel({ exclude: 'node_modules/**', plugins: ['external-helpers'] }),
+    commonjs()
+  ]
 }
-//
-// plugins: [
-//   resolve(),
-//   babel({ exclude: 'node_modules/**', plugins: ['external-helpers'] }),
-//   commonjs()
-// ],
+
 // external: [
 //   'emotion',
 //   'emotion-theming',
 //   'polished',
+
 //   'prop-types',
 //   'react',
 //   'react-dom',
